954193d92a8e43a1339923d6414b81b6124a9a12
jfoste81 <jfoste81@uncc.edu>
2023-11-02 02:29:07 -0400

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Joshua Foster on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
